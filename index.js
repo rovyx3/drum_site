2228b00549f42cca95b20ff33539690d017992ab
Igor Forenc <71031000+rovyx3@users.noreply.github.com>
2021-04-21 19:22:25 +0200

    full-working site

--- index.js
+++ index.js
@@ -1,21 +1,67 @@
+// Buttons
+var buttons = document.querySelectorAll(".drum").length;
 
-for (var n=0; n<7; n++) {
+for (var n=0; n<buttons; n++) {
   document.querySelectorAll(".drum")[n].addEventListener("click", function() {
-    var wDrumAudio = new Audio("sounds/tom-1.mp3");
-    wDrumAudio.play();
+    var letter = this.innerHTML;
+    makeSound(letter);
+    clickAnimation(letter);
   });
 }
 
-// var wDrumAudio = new Audio("tom-1.mp3");
-//
-// var aDrumAudio = new Audio("tom-2.mp3");
-//
-// var sDrumAudio = new Audio("tom-3.mp3");
-//
-// var dDrumAudio = new Audio("tom-4.mp3");
-//
-// var jDrumAudio = new Audio("crash.mp3");
-//
-// var kDrumAudio = new Audio("kick-bass.mp3");
-//
-// var lDrumAudio = new Audio("snare.mp3");
+// Keyboard
+document.addEventListener("keydown", function(event){
+  makeSound(event.key);   // .key reffers directly to the value of key pressed on keaybord (it tells us which keyboard key was pressed)
+  clickAnimation(event.key);
+});
+
+
+
+function makeSound(key){
+
+  switch (key) {
+    case "w":
+      var w_audio = new Audio("sounds/tom-1.mp3");
+      w_audio.play();
+    break;
+
+    case "a":
+      var a_audio = new Audio("sounds/tom-2.mp3");
+      a_audio.play();
+    break;
+
+    case "s":
+      var s_audio = new Audio("sounds/tom-3.mp3");
+      s_audio.play();
+    break;
+
+    case "d":
+      var d_audio = new Audio("sounds/tom-4.mp3");
+      d_audio.play();
+    break;
+
+    case "j":
+      var j_audio = new Audio("sounds/snare.mp3");
+      j_audio.play();
+    break;
+
+    case "k":
+      var k_audio = new Audio("sounds/crash.mp3");
+      k_audio.play();
+    break;
+
+    case "l":
+      var l_audio = new Audio("sounds/kick-bass.mp3");
+      l_audio.play();
+    break;
+
+    default: console.log(key);
+  }
+}
+
+
+function clickAnimation(currentKey){
+  var clicked = document.querySelector("." + currentKey);
+  clicked.classList.add("pressed");
+  setTimeout(function(){clicked.classList.remove("pressed");}, 100);
+}
